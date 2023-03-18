75feb6760ee040802abe4089f04be1e7df5340c3
Clowie Silubrico <clowiesilubrico@gmail.com>
2023-03-18 12:33:36 +0800

    Remove unused import

--- src/main/java/clone/twitter/exception/GenericHandler.java
+++ src/main/java/clone/twitter/exception/GenericHandler.java
@@ -3,8 +3,6 @@ package clone.twitter.exception;
 import clone.twitter.model.exception.Error;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,7 +14,6 @@ import java.util.Date;
 import static clone.twitter.constant.ExceptionConstants.GenericExceptionConstants.UNKNOWN_ERROR;
 
 @ControllerAdvice
-@Order(Ordered.LOWEST_PRECEDENCE)
 public final class GenericHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GenericHandler.class);
