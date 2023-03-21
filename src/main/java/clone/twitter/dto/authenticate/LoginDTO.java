2e89c222688b994d516c3f389b5640ae98e395fa
Clowie Silubrico <clowiesilubrico@gmail.com>
2023-03-21 23:45:34 +0800

    Update toString

--- src/main/java/clone/twitter/dto/authenticate/LoginDTO.java
+++ src/main/java/clone/twitter/dto/authenticate/LoginDTO.java
@@ -3,10 +3,12 @@ package clone.twitter.dto.authenticate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString(exclude = "password")
 public class LoginDTO {
     private String emailOrHandle;
     private String password;
