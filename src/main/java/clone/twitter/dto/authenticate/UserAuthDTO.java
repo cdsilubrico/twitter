dce0a467e00160ad85358c2dcd2971cf8d642566
Clowie Silubrico <clowiesilubrico@gmail.com>
2023-04-24 23:55:03 +0800

    Add not empty validation

--- src/main/java/clone/twitter/dto/authenticate/UserAuthDTO.java
+++ src/main/java/clone/twitter/dto/authenticate/UserAuthDTO.java
@@ -7,15 +7,20 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @ToString(exclude = {"password"})
 @NoArgsConstructor
 public class UserAuthDTO {
     private Long accountId;
+    @NotEmpty(message = "Email field can't be empty.")
     private String email;
+    @NotEmpty(message = "Handle field can't be empty.")
     private String handle;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotEmpty(message = "Password field can't be empty.")
     private String password;
 
     public UserAuthDTO(final UserAuth userAuth) {
