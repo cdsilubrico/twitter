e4de59ee86436ccf6bae30a28bbd45f7208acde2
Clowie Silubrico <clowiesilubrico@gmail.com>
2023-04-24 12:20:48 +0800

    Update swagger function name

--- src/main/java/clone/twitter/config/Swagger.java
+++ src/main/java/clone/twitter/config/Swagger.java
@@ -16,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @NoArgsConstructor
 public class Swagger {
     @Bean
-    public Docket sample() {
+    public Docket swagger() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("clone.twitter"))
