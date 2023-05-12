6b684586c3d6c9f745ddebdbaff850ed09fe0e21
Clowie Silubrico <clowiesilubrico@gmail.com>
2023-05-12 17:22:43 +0800

    Fix pmd issues

--- src/main/java/clone/twitter/exception/specificException/DuplicateEntry.java
+++ src/main/java/clone/twitter/exception/specificException/DuplicateEntry.java
@@ -2,7 +2,13 @@ package clone.twitter.exception.specificException;
 
 import org.springframework.dao.DataIntegrityViolationException;
 
+import java.io.Serial;
+
 public class DuplicateEntry extends DataIntegrityViolationException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     public DuplicateEntry(final String msg) {
         super(msg);
     }
--- src/main/java/clone/twitter/exception/specificException/NoRecordFound.java
+++ src/main/java/clone/twitter/exception/specificException/NoRecordFound.java
@@ -1,8 +1,12 @@
 package clone.twitter.exception.specificException;
 
+import java.io.Serial;
 import java.util.NoSuchElementException;
 
 public class NoRecordFound extends NoSuchElementException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     public NoRecordFound(final String msg) {
         super(msg);
     }
