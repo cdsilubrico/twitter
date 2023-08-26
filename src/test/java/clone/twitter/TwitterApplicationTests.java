107b8109ee749e1d100dd50d0376f675d072edfe
Clowie Silubrico <clowiesilubrico@gmail.com>
2023-08-26 19:12:07 +0800

    Unit test main app

--- src/test/java/clone/twitter/TwitterApplicationTests.java
+++ src/test/java/clone/twitter/TwitterApplicationTests.java
@@ -1,13 +1,21 @@
 package clone.twitter;
 
+import clone.twitter.controller.auth.AuthController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TwitterApplicationTests {
 
+	@Autowired
+	private AuthController authController;
+
 	@Test
 	void contextLoads() {
+		assertThat(this.authController).isNotNull();
 	}
 
 }
